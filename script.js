837127e29eec77c1ad5707e5cf51b631210e0f32
Micah Cowan <micah@addictivecode.org>
2016-05-19 21:31:20 -0700

    Guess the answer.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 let info = {
   q: "Does it have flippers?",
   y: {animal: "a dolphin"},
@@ -6,13 +7,19 @@ let info = {
 let states = {
   start: {
     show: 'h1, .beginRestart'
-  , enter: () => {
-      question = info;
-      $('.beginRestart button').html("Begin");
+  , enter() {
+      setQuestion(info)
+      $('.beginRestart button').text("Begin");
     }
   }
 , ask: {
     show: 'h1, .beginRestart, .question'
+  }
+, guess: {
+    show: 'h1, .beginRestart, .guess'
+  , enter() {
+      $('.guess p span').text(question.animal)
+    }
   }
 }
 
@@ -23,23 +30,41 @@ setupEvents();
 gotoState('start')
 
 function setupEvents() {
+  // Begin button
   $('.beginRestart button').click(() => {
     if (state == 'start') {
-      $('.beginRestart button').html('Restart');
+      $('.beginRestart button').text('Restart');
       gotoState('ask');
     }
     else
       gotoState('start');
   })
+
+  // Answer buttons
+  $('.question button').click(function() {
+    var answer = $(this).text().substring(0,1).toLowerCase();
+    setQuestion(question[answer]);
+  })
 }
 
 function gotoState(s) {
   let newS = states[s];
-  
+
+  // call enter
+  if (newS.enter) newS.enter();
+
   // hide/show things
   if (state !== undefined)
-    $(state.show).not(newS.show).slideUp();
+    $(states[state].show).not(newS.show).slideUp();
   $(newS.show).slideDown();
-  
+
   state = s;
+}
+
+function setQuestion(q) {
+  question = q;
+  if (q.q)
+    $('.question p').text(q.q);
+  else
+    gotoState('guess')
 }
